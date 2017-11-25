cc63507e8362aaa272953bd78f902261b5e1b530
Lester <lester.crespo.es@gmail.com>
2017-11-25 09:08:40 +0000

    First version of string hash

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include "stdio.h"
+#include <type_traits>
+#include <stdint.h>
+
+static constexpr size_t InitialFNV = 2166136261U;
+static constexpr size_t FNVMultiple = 16777619;
+
+#define GET_SZIE(str) sizeof(str)
+
+#define GET_HASH(str)
+
+template<class T>
+class StringHash
+{
+public:
+	template<unsigned prev, unsigned pos>
+	static constexpr uint64_t compute()
+	{
+		return (prev ^ T::template get<pos>()) * FNVMultiple;
+	}
+
+	template<uint64_t prev, unsigned pos, unsigned len>
+	static constexpr typename std::enable_if<(pos < len), uint64_t>::type hash()
+	{
+		return hash< compute<prev, pos>(), pos + 1, len>();
+	}
+
+	template<uint64_t prev, unsigned pos, unsigned len>
+	static constexpr typename std::enable_if<(pos == len), uint64_t>::type hash()
+	{
+		return prev;
+	}
+
+	static constexpr uint64_t get()
+	{
+		return hash<InitialFNV, 0, T::len>();
+	}
+
+};
+
+class StringReject
+{
+public:
+	template<unsigned pos>
+	static constexpr uint64_t get()
+	{
+		return "Reject"[pos];
+	}
+	static const unsigned len = sizeof("Reject") - 1;
+	//static const unsigned hash = StringHash<StringReject>::get();
+};
+
+template<class type>
+void fnc(type* v)
+{
+
+}
+
+template<class type>
+void fnc(type v)
+{
+
+}
+
+int main()
+{
+
+	const static uint64_t a = StringHash<StringReject>::get();
+	printf("%lu", a);
+	return 0;
+}
+
